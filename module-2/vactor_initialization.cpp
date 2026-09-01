188feafa4334565b49f8288a6737d63b41d3ad80
rifatmotain <rifatmotain@gmail.com>
2026-09-01 23:12:45 +0600

    add

---
+++ module-2/vactor_initialization.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+//vector <int> v;
+//vector <int> v(10);
+//vector <int> v(10,-1);
+//     for (int i=0; i<v.size(); i++)
+//     {
+//         cout << v[i] << " ";
+//     }
+// }
+// vector <int> v2(v);
+// for (int i=0; i<v2.size(); i++)
+//     {
+//         cout << v2[i] << " ";
+//     }
+// int a[]={1,2,3,4};
+// vector <int> v (a,a+4);
+// for (int i=0; i<4; i++)
+//     {
+//         cout << v[i] << " ";
+//     }
+
+}
